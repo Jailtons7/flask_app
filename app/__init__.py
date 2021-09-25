ffdbf26241665a6b10b76215f1f3b56cb00e494d
Jailton Silva <jss1@ifal.edu.br>
2021-09-24 22:14:28 -0300

    feat: adding db connection via SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 from app.views import HelloWorld
+from tutorial.config import Config
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
     api = Api(app)
     api.add_resource(HelloWorld, '/')
+    db.init_app(app)
     return app
