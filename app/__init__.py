0f9726ae8e250f97f3f2a66a4b7d2eea5490820b
Jailton Silva <jss1@ifal.edu.br>
2021-09-25 21:30:50 -0300

    feat: adding database migrations to application

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+from app.models import db
 from app.views import HelloWorld
 from tutorial.config import Config
 
-db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -16,4 +17,5 @@ def create_app():
     api = Api(app)
     api.add_resource(HelloWorld, '/')
     db.init_app(app)
+    migrate.init_app(app, db)
     return app
